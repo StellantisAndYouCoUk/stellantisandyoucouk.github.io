bce8edabea34c61c75ae7d3046d1f1ed7b4bcc0d
JasonSLe <107549434+JasonSLe@users.noreply.github.com>
2024-04-10 09:12:22 +0100

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -4382,10 +4382,10 @@ $(document).on('knack-view-render.view_3278', function (event, view, data) {
     $('th[class="field_1532"]').hide();
     $('td[class*="field_1532"]').hide();
 	
-	 tooltipsTable('1098','3805','field_1532','field_2586');
+	 tooltipsTable('1050','3805','field_1532','field_2586');
 	//scene, view, hover info, field to have hover
    	
-	tooltipsTable('1098','3805','field_1537','field_2213');  
+	tooltipsTable('1050','3805','field_1537','field_2213');  
 	    
     });
 
