2faa1b468d3da9826d66dd524c257130258d4b32
Hynek <hynek@hruska.cz>
2024-06-21 21:35:44 +0200

    dev

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -287,10 +287,10 @@ function stopLoading(viewID){
 var currentRefreshScene = [];
 function recursiveSceneRefresh(sceneId,viewsArray,refreshInterval, runCount = 0){
   console.log('recursiveSceneRefresh',sceneId,runCount)
-  if (currentRefreshScene.includes(sceneId) && runCount === 0) {console.log('new refresh of same scene, exiting'); return;}
+  if (currentRefreshScene.includes(sceneId) && runCount === 0) {console.log('refresh '+sceneId+',new refresh of same scene, exiting'); return;}
   currentRefreshScene.push(sceneId);
   setTimeout(function () { 
-    if ($('div[id="kn-scene_'+sceneId+'"]').length===0) {console.log('another scene, stop refresh'); currentRefreshScene = currentRefreshScene.filter(el => el !== sceneId); return;} 
+    if ($('div[id="kn-scene_'+sceneId+'"]').length===0) {console.log('refresh '+sceneId+', another scene, stop refresh'); currentRefreshScene = currentRefreshScene.filter(el => el !== sceneId); return;} 
     for (let i = 0;i<viewsArray.length;i++){
       if($("#"+viewsArray[i]+"").is(":visible")==true) Knack.views[viewsArray[i]].model.fetch();
     }
