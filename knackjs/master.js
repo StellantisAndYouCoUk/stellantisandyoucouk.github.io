c0ec0ccafe32615f4f593ac2715bc896f6a0c39c
JasonSLe <107549434+JasonSLe@users.noreply.github.com>
2024-06-25 13:31:03 +0100

    Update master.js

--- knackjs/master.js
+++ knackjs/master.js
@@ -460,11 +460,18 @@ $(document).on('knack-view-render.view_6742', function(event, view, data) {
   $('div[class*="field_3"]').html('<iframe src="https://www.stellantisandyou.co.uk/digital-orders?token='+encodeURIComponent(token) + ' #new-vehicle-invoice-reporting" allow="camera" frameborder="0" width="100%" id="knack-iframe"></iframe>');
 });
 
+//Job Card Archive
+$(document).on('knack-view-render.view_7139', function(event, view, data) {
+  var token = Knack.getUserAttributes().values["field_6440"];
+  $('div[class*="field_3"]').html('<iframe src="https://www.stellantisandyou.co.uk/aftersales#job-card-archive/?token='+encodeURIComponent(token) + '" allow="camera" frameborder="0" width="100%" id="knack-iframe"></iframe>');
+});
+
 
 
 var aftersalesConnectView = [{view:'view_6320',url:'#technician-view-my-jobs-v2'},
 //{view:'view_6483',url:'#workshop-control/'},
 			     {view:'view_6510',url:'#aftersales-key-tag-search'},
+/*Job Card Archive*/ {view:'view_7139',url:'#job-card-archive/'},
 {view:'view_5733',url:'#after-sales-vehicle-lookup/'},
 {view:'view_5734',url:'#after-sales-vehicle-lookup/pre-visit/'},
 {view:'view_5735',url:'#after-sales-vehicle-lookup/check-in/'},
