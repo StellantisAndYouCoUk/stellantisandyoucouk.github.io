dff4e271a63df60d5e7bf2c9ce7d960404d371bc
DanielCheng1 <118259318+DanielCheng1@users.noreply.github.com>
2024-07-03 16:54:34 +0100

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -4894,8 +4894,3 @@ $(document).on('knack-form-submit.view_3161', function(event, view, data) {
     }
   }
 });*/
-
-$(document).on('knack-form-submit.view_1530', function(event, view, data) {
-if (data.field_2937 === "Yes")	
-$(‘html, body’).animate({ scrollTop: $(document).height() }, ‘fast’);
-});
