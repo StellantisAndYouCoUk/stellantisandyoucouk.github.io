cf3a47ad75744f7fac85ee90a350319e56c000f1
makifyeginS <mehmet.yegin@stellantis.com>
2024-08-04 00:26:04 +0100

    dev
    dev

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -4957,7 +4957,47 @@ $(document).on('knack-view-render.view_3773', function(event, view, data) {
               $('body').append(partsModelHTML);
           
               // Show the modal
+              $('#myModal').modal({
+                keyboard: true
+              })
               $('#myModal').modal('show');
+
+
+              $('#searchButton').on('click', function() {
+                Swal.fire({
+                    title: "Submit your GitHub username",
+                    input: "text",
+                    inputAttributes: {
+                        autocapitalize: "off"
+                    },
+                    showCancelButton: true,
+                    confirmButtonText: "Look up",
+                    showLoaderOnConfirm: true,
+                    preConfirm: async (login) => {
+                        try {
+                            const githubUrl = `https://api.github.com/users/${login}`;
+                            const response = await fetch(githubUrl);
+                            if (!response.ok) {
+                                return Swal.showValidationMessage(`Request failed: ${response.statusText}`);
+                            }
+                            return response.json();
+                        } catch (error) {
+                            Swal.showValidationMessage(`Request failed: ${error}`);
+                        }
+                    },
+                    allowOutsideClick: () => !Swal.isLoading()
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        Swal.fire({
+                            title: `${result.value.login}'s avatar`,
+                            imageUrl: result.value.avatar_url
+                        });
+                    }
+                });
+            });
+
+
+
           
               // Remove the modal from the DOM when it's closed to prevent clutter
               $('#myModal').on('hidden.bs.modal', function() {
