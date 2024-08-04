4e6cff19aea08e57dc1f5fb75f0b0f6fa0924072
makifyeginS <mehmet.yegin@stellantis.com>
2024-08-04 13:49:58 +0100

    Update aftersales.js
    dev

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -5021,106 +5021,7 @@ $(document).on('knack-form-submit.view_3161', function(event, view, data) {
 // });
 
 
-// $(document).on('knack-view-render.view_3773', function(event, view, data) {
-//   console.log("View render event triggered");
-
-//   // Function to load a script and return a promise
-//   function loadScript(src) {
-//     return new Promise((resolve, reject) => {
-//       const script = document.createElement('script');
-//       script.src = src + '?cache-bust=' + new Date().getTime(); // Add cache-busting
-//       script.onload = () => resolve(script);
-//       script.onerror = () => reject(new Error(`Script load error for ${src}`));
-//       document.head.append(script);
-//     });
-//   }
-  
-//   // Load jQuery first
-//   loadScript('https://code.jquery.com/jquery-3.5.1.min.js').then(() => {
-//     // Load other scripts after jQuery is loaded
-//     return Promise.all([
-//       loadScript('https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js'),
-//       loadScript('https://unpkg.com/htmx.org@1.8.3'),
-//       loadScript('https://cdn.jsdelivr.net/npm/sweetalert2@11.12.4/dist/sweetalert2.all.min.js')
-//     ]);
-//   }).then(() => {
-//     // Append CSS files after all scripts are loaded
-//     $('head').append('<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" type="text/css" />');
-//     $('head').append('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.12.4/dist/sweetalert2.min.css" type="text/css" />');
-
-  
-
-//     // Add Modal Structure to the body
-//     $(document).on('mouseleave', '.fa-cart-arrow-down', function() {
-//       // Remove any existing modal with the same ID to prevent duplicates
-//       $('#myModal').remove();
-
-//       // Append the modal structure to the body
-//       let partsModelHTML = '';
-//       if (partsModelHTML === '') {
-//         partsModelHTML = $.ajax({
-//           type: "GET",
-//           url: 'https://stellantisandyoucouk.github.io/modalHTML/modal.html',
-//           cache: false,
-//           async: false
-//         }).responseText;
-//       }
-//       $('body').append(partsModelHTML);
-
-//       // Show the modal
-//       $('#myModal').modal({
-//         keyboard: true,
-//         show: true,
-//         handleUpdate: true
-//       });
-
-//       $('#searchButton').on('click', function() {
-//         Swal.fire({
-//           title: "Bin is Available",
-//           input: "text",
-//           inputAttributes: {
-//             autocapitalize: "on"
-//           },
-//           showCancelButton: true,
-//           confirmButtonText: "Look up",
-//           showLoaderOnConfirm: true,
-//           preConfirm: async (login) => {
-//             try {
-//               const githubUrl = `https://api.github.com/users/${login}`;
-//               const response = await fetch(githubUrl);
-//               if (!response.ok) {
-//                 return Swal.showValidationMessage(`Request failed: ${response.statusText}`);
-//               }
-//               return response.json();
-//             } catch (error) {
-//               Swal.showValidationMessage(`Request failed: ${error}`);
-//             }
-//           },
-//           allowOutsideClick: () => !Swal.isLoading()
-//         }).then((result) => {
-//           if (result.isConfirmed) {
-//             Swal.fire({
-//               title: `${result.value.login}'s avatar`,
-//               imageUrl: result.value.avatar_url
-//             });
-//           }
-//         });
-//       });
-
-//       // Remove the modal from the DOM when it's closed to prevent clutter
-//       $('#myModal').on('hidden.bs.modal', function() {
-//         $(this).remove();
-//       });
-
-//       console.log("Mouse leave detected");
-//     });
-//   }).catch((error) => {
-//     console.error("Failed to load scripts:", error);
-//   });
-// });
-
-
-document.addEventListener('knack-view-render.view_3773', function(event) {
+$(document).on('knack-view-render.view_3773', function(event, view, data) {
   console.log("View render event triggered");
 
   // Function to load a script and return a promise
@@ -5133,95 +5034,89 @@ document.addEventListener('knack-view-render.view_3773', function(event) {
       document.head.append(script);
     });
   }
-
-  // Load other scripts after jQuery is loaded
-  Promise.all([
-    loadScript('https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js'),
-    loadScript('https://unpkg.com/htmx.org@1.8.3'),
-    loadScript('https://cdn.jsdelivr.net/npm/sweetalert2@11.12.4/dist/sweetalert2.all.min.js')
-  ]).then(() => {
+  
+  // Load jQuery first
+  loadScript('https://code.jquery.com/jquery-3.5.1.min.js').then(() => {
+    // Load other scripts after jQuery is loaded
+    return Promise.all([
+      loadScript('https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js'),
+      loadScript('https://unpkg.com/htmx.org@1.8.3'),
+      loadScript('https://cdn.jsdelivr.net/npm/sweetalert2@11.12.4/dist/sweetalert2.all.min.js')
+    ]);
+  }).then(() => {
     // Append CSS files after all scripts are loaded
-    const head = document.head;
+    $('head').append('<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" type="text/css" />');
+    $('head').append('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.12.4/dist/sweetalert2.min.css" type="text/css" />');
 
-    const bootstrapCSS = document.createElement('link');
-    bootstrapCSS.rel = 'stylesheet';
-    bootstrapCSS.href = 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css';
-    head.append(bootstrapCSS);
-
-    const sweetAlertCSS = document.createElement('link');
-    sweetAlertCSS.rel = 'stylesheet';
-    sweetAlertCSS.href = 'https://cdn.jsdelivr.net/npm/sweetalert2@11.12.4/dist/sweetalert2.min.css';
-    head.append(sweetAlertCSS);
+  
 
     // Add Modal Structure to the body
-    document.addEventListener('mouseleave', function(event) {
-      if (event.target.classList.contains('fa-cart-arrow-down')) {
-        // Remove any existing modal with the same ID to prevent duplicates
-        const existingModal = document.getElementById('myModal');
-        if (existingModal) {
-          existingModal.remove();
-        }
+    $(document).on('mouseleave', '.fa-cart-arrow-down', function() {
+      // Remove any existing modal with the same ID to prevent duplicates
+      $('#myModal').remove();
 
-        // Append the modal structure to the body
-        fetch('https://stellantisandyoucouk.github.io/modalHTML/modal.html')
-          .then(response => response.text())
-          .then(partsModelHTML => {
-            document.body.insertAdjacentHTML('beforeend', partsModelHTML);
-
-            // Show the modal
-            const modalElement = document.getElementById('myModal');
-            const modal = new bootstrap.Modal(modalElement, {
-              keyboard: true,
-              show: true,
-              handleUpdate: true
-            });
-            modal.show();
-
-            const searchButton = document.getElementById('searchButton');
-            searchButton.addEventListener('click', function() {
-              Swal.fire({
-                title: "Bin is Available",
-                input: "text",
-                inputAttributes: {
-                  autocapitalize: "on"
-                },
-                showCancelButton: true,
-                confirmButtonText: "Look up",
-                showLoaderOnConfirm: true,
-                preConfirm: async (login) => {
-                  try {
-                    const githubUrl = `https://api.github.com/users/${login}`;
-                    const response = await fetch(githubUrl);
-                    if (!response.ok) {
-                      return Swal.showValidationMessage(`Request failed: ${response.statusText}`);
-                    }
-                    return response.json();
-                  } catch (error) {
-                    Swal.showValidationMessage(`Request failed: ${error}`);
-                  }
-                },
-                allowOutsideClick: () => !Swal.isLoading()
-              }).then((result) => {
-                if (result.isConfirmed) {
-                  Swal.fire({
-                    title: `${result.value.login}'s avatar`,
-                    imageUrl: result.value.avatar_url
-                  });
-                }
-              });
-            });
-
-            // Remove the modal from the DOM when it's closed to prevent clutter
-            modalElement.addEventListener('hidden.bs.modal', function() {
-              this.remove();
-            });
-
-            console.log("Mouse leave detected");
-          })
-          .catch(error => console.error("Failed to load modal HTML:", error));
+      // Append the modal structure to the body
+      let partsModelHTML = '';
+      if (partsModelHTML === '') {
+        partsModelHTML = $.ajax({
+          type: "GET",
+          url: 'https://stellantisandyoucouk.github.io/modalHTML/modal.html',
+          cache: false,
+          async: false
+        }).responseText;
       }
+      $('body').append(partsModelHTML);
+
+      // Show the modal
+      $('#myModal').modal({
+        keyboard: true,
+        show: true,
+        handleUpdate: true
+      });
+
+      $('#searchButton').on('click', function() {
+        Swal.fire({
+          title: "Bin is Available",
+          input: "text",
+          inputAttributes: {
+            autocapitalize: "on"
+          },
+          showCancelButton: true,
+          confirmButtonText: "Look up",
+          showLoaderOnConfirm: true,
+          preConfirm: async (login) => {
+            try {
+              const githubUrl = `https://api.github.com/users/${login}`;
+              const response = await fetch(githubUrl);
+              if (!response.ok) {
+                return Swal.showValidationMessage(`Request failed: ${response.statusText}`);
+              }
+              return response.json();
+            } catch (error) {
+              Swal.showValidationMessage(`Request failed: ${error}`);
+            }
+          },
+          allowOutsideClick: () => !Swal.isLoading()
+        }).then((result) => {
+          if (result.isConfirmed) {
+            Swal.fire({
+              title: `${result.value.login}'s avatar`,
+              imageUrl: result.value.avatar_url
+            });
+          }
+        });
+      });
+
+      // Remove the modal from the DOM when it's closed to prevent clutter
+      $('#myModal').on('hidden.bs.modal', function() {
+        $(this).remove();
+      });
+
+      console.log("Mouse leave detected");
     });
   }).catch((error) => {
     console.error("Failed to load scripts:", error);
   });
 });
+
+
