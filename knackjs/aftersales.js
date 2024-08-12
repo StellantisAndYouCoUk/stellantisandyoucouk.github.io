4c20e4011e9e367c680b7a8a482582d675772b3f
JasonSLe <107549434+JasonSLe@users.noreply.github.com>
2024-08-12 13:44:21 +0100

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -4923,7 +4923,7 @@ $(document).on('knack-view-render.view_3773', function(event, view, data) {
 
 
 
-  // Function to load a script and return a promise
+/*  // Function to load a script and return a promise
   function loadScript(src) {
     return new Promise((resolve, reject) => {
       const script = document.createElement('script');
@@ -5084,6 +5084,7 @@ $(document).on('knack-view-render.view_3773', function(event, view, data) {
   });
 });
 
+*/
 
 // $(document).on('knack-view-render.view_4230', function(event, scene) {
 // $('#view_4230').css('flex','0')
