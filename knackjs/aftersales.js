f9fb395ed292f233623dbdd25a92faf4825ea21b
JasonSLe <107549434+JasonSLe@users.noreply.github.com>
2024-08-12 13:51:50 +0100

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -4917,11 +4917,11 @@ $(document).on('knack-form-submit.view_3161', function(event, view, data) {
 
 
 
-$(document).on('knack-view-render.view_3773', function(event, view, data) {
+/*$(document).on('knack-view-render.view_3773', function(event, view, data) {
   $('.field_3181').hide();
   console.log("View render event triggered");
 
-});
+});*/
 
  // Function to load a script and return a promise
   function loadScript(src) {
