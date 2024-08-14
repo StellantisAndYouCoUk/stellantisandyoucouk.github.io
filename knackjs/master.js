023e25e635f3c9d4280e3f9c29a9769dec4b2397
makifyeginS <mehmet.yegin@stellantis.com>
2024-08-14 13:56:47 +0100

    Update master.js
    dev

--- knackjs/master.js
+++ knackjs/master.js
@@ -4954,9 +4954,10 @@ function createLink(url, linkText){
 $(document).on('knack-view-render.any', function(event, scene) {
 
   const userAttributes = Knack.getUserAttributes();
+   if(userAttributes){
   const userValue = userAttributes.values.field_7974 || null;
 
-  if(userValue){
+ 
   subscribeURL = `https://ntfy.sh/DMRzyZwTVWz46Fy86blfD1G1TAL-${userValue}/sse`;
 
   const eventSource = new EventSource(subscribeURL);
