5f3bdc6a086b0e122497214ed05de476a8ab1145
makifyeginS <mehmet.yegin@stellantis.com>
2024-08-14 11:27:36 +0100

    Update master.js
    dev

--- knackjs/master.js
+++ knackjs/master.js
@@ -4966,7 +4966,8 @@ function createLink(url, linkText){
           Swal.fire({
             title: parsedData.title ||'No Title',
             text: parsedData.message || 'No Message',
-            icon: "success"
+            icon: "info",
+            allowOutsideClick: false
           });
 
 
