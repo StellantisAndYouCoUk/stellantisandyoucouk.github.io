319d393f86471f6d3dad4ef1071ad348656cd250
makifyeginS <mehmet.yegin@stellantis.com>
2024-08-14 16:59:13 +0100

    Update master.js
    dev

--- knackjs/master.js
+++ knackjs/master.js
@@ -5048,7 +5048,43 @@ function createLink(url, linkText){
 
  $(document).on('knack-scene-render.scene_2297', function(event, scene){
 
-console.log("scene_2297")
+  Swal.fire({
+    title: "Submit your Github username",
+    input: "text",
+    inputAttributes: {
+      autocapitalize: "off"
+    },
+    showCancelButton: true,
+    confirmButtonText: "Look up",
+    showLoaderOnConfirm: true,
+    preConfirm: async (login) => {
+      try {
+        const githubUrl = `
+          https://api.github.com/users/${login}
+        `;
+        const response = await fetch(githubUrl);
+        if (!response.ok) {
+          return Swal.showValidationMessage(`
+            ${JSON.stringify(await response.json())}
+          `);
+        }
+        return response.json();
+      } catch (error) {
+        Swal.showValidationMessage(`
+          Request failed: ${error}
+        `);
+      }
+    },
+    allowOutsideClick: () => !Swal.isLoading()
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        title: `${result.value.login}'s avatar`,
+        imageUrl: result.value.avatar_url
+      });
+    }
+  });
+  
 
 
  })
