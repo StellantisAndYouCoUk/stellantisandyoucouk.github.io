91100e117d08fac786059be851dffd1fe5be02ce
makifyeginS <mehmet.yegin@stellantis.com>
2024-08-14 13:57:22 +0100

    Update master.js
    dev

--- knackjs/master.js
+++ knackjs/master.js
@@ -4957,6 +4957,7 @@ $(document).on('knack-view-render.any', function(event, scene) {
    if(userAttributes){
   const userValue = userAttributes.values.field_7974 || null;
 
+  if(userValue){
  
   subscribeURL = `https://ntfy.sh/DMRzyZwTVWz46Fy86blfD1G1TAL-${userValue}/sse`;
 
@@ -4984,5 +4985,5 @@ $(document).on('knack-view-render.any', function(event, scene) {
       showNotification(e.data);
   };
 }
-
+   }
 })
