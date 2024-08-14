0c81695bd06607acdc33d0869700f83dbebb64dc
makifyeginS <mehmet.yegin@stellantis.com>
2024-08-14 17:11:30 +0100

    Update master.js
    dev

--- knackjs/master.js
+++ knackjs/master.js
@@ -5046,60 +5046,60 @@ function createLink(url, linkText){
  })
 
 
- $(document).on('knack-scene-render.scene_2297', function(event, scene){
+//  $(document).on('knack-scene-render.scene_2297', function(event, scene){
 
-  var $button = $('<button>', {
-    id: 'githubButton',
-    type: 'button',
-    text: 'Send Messages'
-});
+//   var $button = $('<button>', {
+//     id: 'githubButton',
+//     type: 'button',
+//     text: 'Send Messages'
+// });
 
-// Append the button to a container
-$('#buttonContainer').append($button);
-
-
-
-  $('#githubButton').on('click', function() {
+// // Append the button to a container
+// $('#buttonContainer').append($button);
 
 
-  Swal.fire({
-    title: "Submit your Github username",
-    input: "text",
-    inputAttributes: {
-      autocapitalize: "off"
-    },
-    showCancelButton: true,
-    confirmButtonText: "Look up",
-    showLoaderOnConfirm: true,
-    preConfirm: async (login) => {
-      try {
-        const githubUrl = `
-          https://api.github.com/users/${login}
-        `;
-        const response = await fetch(githubUrl);
-        if (!response.ok) {
-          return Swal.showValidationMessage(`
-            ${JSON.stringify(await response.json())}
-          `);
-        }
-        return response.json();
-      } catch (error) {
-        Swal.showValidationMessage(`
-          Request failed: ${error}
-        `);
-      }
-    },
-    allowOutsideClick: () => !Swal.isLoading()
-  }).then((result) => {
-    if (result.isConfirmed) {
-      Swal.fire({
-        title: `${result.value.login}'s avatar`,
-        imageUrl: result.value.avatar_url
-      });
-    }
-  });
+
+//   $('#githubButton').on('click', function() {
+
+
+//   Swal.fire({
+//     title: "Submit your Github username",
+//     input: "text",
+//     inputAttributes: {
+//       autocapitalize: "off"
+//     },
+//     showCancelButton: true,
+//     confirmButtonText: "Look up",
+//     showLoaderOnConfirm: true,
+//     preConfirm: async (login) => {
+//       try {
+//         const githubUrl = `
+//           https://api.github.com/users/${login}
+//         `;
+//         const response = await fetch(githubUrl);
+//         if (!response.ok) {
+//           return Swal.showValidationMessage(`
+//             ${JSON.stringify(await response.json())}
+//           `);
+//         }
+//         return response.json();
+//       } catch (error) {
+//         Swal.showValidationMessage(`
+//           Request failed: ${error}
+//         `);
+//       }
+//     },
+//     allowOutsideClick: () => !Swal.isLoading()
+//   }).then((result) => {
+//     if (result.isConfirmed) {
+//       Swal.fire({
+//         title: `${result.value.login}'s avatar`,
+//         imageUrl: result.value.avatar_url
+//       });
+//     }
+//   });
   
 
 
- })
-});
+//  })
+// });
