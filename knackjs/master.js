1517b09da68517a3f81132b1d2d00b36086e2723
makifyeginS <mehmet.yegin@stellantis.com>
2024-08-14 17:04:30 +0100

    Update master.js
    dev

--- knackjs/master.js
+++ knackjs/master.js
@@ -5042,6 +5042,11 @@ function createLink(url, linkText){
 
  $(document).on('knack-scene-render.scene_2297', function(event, scene){
 
+  <button id="githubButton" type="button">Send Messages</button>
+
+  $('#githubButton').on('click', function() {
+
+
   Swal.fire({
     title: "Submit your Github username",
     input: "text",
@@ -5082,3 +5087,4 @@ function createLink(url, linkText){
 
 
  })
+});
