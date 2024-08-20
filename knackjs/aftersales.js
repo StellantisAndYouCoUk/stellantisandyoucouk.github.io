cd4444e3eeb2f3ee4762038931be1a4b47719e16
makifyeginS <mehmet.yegin@stellantis.com>
2024-08-20 16:20:26 +0100

    Update aftersales.js
    dev

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -4940,68 +4940,66 @@ $(document).on('knack-form-submit.view_3161', function(event, view, data) {
 
 
 
-$(document).on('knack-view-render.view_3773', function(event, view, data) {
-  $('.field_3181').hide();
-  console.log("View render event triggered");
+// $(document).on('knack-view-render.view_3773', function(event, view, data) {
+//   $('.field_3181').hide();
+//   console.log("View render event triggered");
  
 
 
 
- // Function to load a script and return a promise
-  function loadScript(src) {
-    return new Promise((resolve, reject) => {
-      const script = document.createElement('script');
-      script.src = src + '?cache-bust=' + new Date().getTime(); // Add cache-busting
-      script.onload = () => resolve(script);
-      script.onerror = () => reject(new Error(`Script load error for ${src}`));
-      document.head.append(script);
-    });
-  }
-  let trId = '';
-  // Load jQuery first
-  loadScript('https://code.jquery.com/jquery-3.5.1.min.js').then(() => {
-    // Load other scripts after jQuery is loaded
-    console.log("jquerry installed");
-    return Promise.all([
-      loadScript('https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js'),
-      loadScript('https://unpkg.com/htmx.org@1.8.3'),
-      loadScript('https://cdn.jsdelivr.net/npm/sweetalert2@11.12.4/dist/sweetalert2.all.min.js')
-    ]);
-  }).then(() => {
+//  // Function to load a script and return a promise
+//   function loadScript(src) {
+//     return new Promise((resolve, reject) => {
+//       const script = document.createElement('script');
+//       script.src = src + '?cache-bust=' + new Date().getTime(); // Add cache-busting
+//       script.onload = () => resolve(script);
+//       script.onerror = () => reject(new Error(`Script load error for ${src}`));
+//       document.head.append(script);
+//     });
+//   }
+//   let trId = '';
+//   // Load jQuery first
+//   loadScript('https://code.jquery.com/jquery-3.5.1.min.js').then(() => {
+//     // Load other scripts after jQuery is loaded
+//     console.log("jquerry installed");
+//     return Promise.all([
+//       loadScript('https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js'),
+//       loadScript('https://unpkg.com/htmx.org@1.8.3'),
+//       loadScript('https://cdn.jsdelivr.net/npm/sweetalert2@11.12.4/dist/sweetalert2.all.min.js')
+//     ]);
+//   }).then(() => {
     
-    // Append CSS files after all scripts are loaded
-    $('head').append('<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" type="text/css" />');
-    $('head').append('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.12.4/dist/sweetalert2.min.css" type="text/css" />');
+//     // Append CSS files after all scripts are loaded
+//     $('head').append('<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" type="text/css" />');
+//     $('head').append('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.12.4/dist/sweetalert2.min.css" type="text/css" />');
           
-    let modelHtml = '';
+//     let modelHtml = '';
 
 
-    let keyValueStore = '';
-    $('tr').mouseenter(function() {
-      trId = $(this).attr('id');
-    console.log('Mouse is on before mouse leave: ' + trId);
+//     let keyValueStore = '';
+//     $('tr').mouseenter(function() {
+//       trId = $(this).attr('id');
+//     console.log('Mouse is on before mouse leave: ' + trId);
 
-    });
+//     });
 
 
   
 
-$(document).on('mouseleave', '.fa-cart-arrow-down', function() {
+// $(document).on('mouseleave', '.fa-cart-arrow-down', function() {
 
-  keyValueStore = '';
-  $('tr').mouseenter(function() {
-    trId = $(this).attr('id');
-    console.log('Mouse is on: ' + trId);
-});
+//   keyValueStore = '';
+//   $('tr').mouseenter(function() {
+//     trId = $(this).attr('id');
+//     console.log('Mouse is on: ' + trId);
+// });
 
  
 
-      $('#myModal').remove();
-$('.modal-backdrop').remove();
+//       $('#myModal').remove();
+// $('.modal-backdrop').remove();
 
-      let modelHtml = '';
-
-
+//       let modelHtml = '';
 
 
 
@@ -5010,67 +5008,69 @@ $('.modal-backdrop').remove();
 
 
 
-      // Append the modal structure to the body
-      let partsModelHTML = '';
-
-      if(partsModelHTML === ''){
-
-        partsModelHTML = callGetHttpRequest('https://stellantisandyoucouk.github.io/modalHTML/modal.html');
-        $('body').append(partsModelHTML);
-
-      }
 
 
+//       // Append the modal structure to the body
+//       let partsModelHTML = '';
 
-      // if (partsModelHTML === '') {
-      //   partsModelHTML = $.ajax({
-      //     type: "GET",
-      //     url: 'https://stellantisandyoucouk.github.io/modalHTML/modal.html',
-      //     cache: false,
-      //     async: false
-      //   }).responseText;
-      // }
-      // $('body').append(partsModelHTML);
+//       if(partsModelHTML === ''){
 
+//         partsModelHTML = callGetHttpRequest('https://stellantisandyoucouk.github.io/modalHTML/modal.html');
+//         $('body').append(partsModelHTML);
 
-      // Show the modal
-      $('#myModal').modal({
-        keyboard: true,
-        show: true,
-        handleUpdate: true
-      });
+//       }
 
 
 
-      Knack.views.view_3773.model.data.models.forEach((model)=>{
+//       // if (partsModelHTML === '') {
+//       //   partsModelHTML = $.ajax({
+//       //     type: "GET",
+//       //     url: 'https://stellantisandyoucouk.github.io/modalHTML/modal.html',
+//       //     cache: false,
+//       //     async: false
+//       //   }).responseText;
+//       // }
+//       // $('body').append(partsModelHTML);
 
-        if(model.id===trId){
-          keyValueStore = model.attributes.field_3181_raw
-        }
 
-      })
-      // const key_value_store = JSON.parse(Knack.views['view_3773'].model.attributes['field_250'])
+//       // Show the modal
+//       $('#myModal').modal({
+//         keyboard: true,
+//         show: true,
+//         handleUpdate: true
+//       });
 
-      let payload = {
-        "keyValueStoreId": keyValueStore,
-        "WIP": 64046,
-        "AccountNumber": "B0208"
-      }
+
+
+//       Knack.views.view_3773.model.data.models.forEach((model)=>{
+
+//         if(model.id===trId){
+//           keyValueStore = model.attributes.field_3181_raw
+//         }
+
+//       })
+//       // const key_value_store = JSON.parse(Knack.views['view_3773'].model.attributes['field_250'])
+
+//       let payload = {
+//         "keyValueStoreId": keyValueStore,
+//         "WIP": 64046,
+//         "AccountNumber": "B0208"
+//       }
       
       
-            const invoice = callPostHttpRequest("https://hook.eu1.make.celonis.com/njmr76ctfodft44xtbo77uy3bvbzq287", payload,"Servicebox invoice find");
+//             const invoice = callPostHttpRequest("https://hook.eu1.make.celonis.com/njmr76ctfodft44xtbo77uy3bvbzq287", payload,"Servicebox invoice find");
       
    
             
 
     
 
-      if(modelHtml==='' && keyValueStore != ''){
+//       if(modelHtml==='' && keyValueStore != ''){
 
-     const response = callGetHttpRequest(`https://api.apify.com/v2/key-value-stores/${keyValueStore}/records/test.html`);
-      $('#akif').append(response);
+//      const response = callGetHttpRequest(`https://api.apify.com/v2/key-value-stores/${keyValueStore}/records/test.html`);
+//       $('#akif').append(response);
 
-      }
+//       }
 
 
 
@@ -5094,54 +5094,54 @@ $('.modal-backdrop').remove();
 
 
 
-      $('#searchButton').on('click', function() {
-        Swal.fire({
-          title: "Bin is Available",
-          input: "text",
-          inputAttributes: {
-            autocapitalize: "on"
-          },
-          showCancelButton: true,
-          confirmButtonText: "Find",
-          showLoaderOnConfirm: true,
-          preConfirm: async (login) => {
-            try {
-              const githubUrl = `https://odbc.robinsandday.co.uk/ExecuteSql`;
-              const response = await fetch(githubUrl);
-              if (!response.ok) {
-                return Swal.showValidationMessage(`Request failed: ${response.statusText}`);
-              }
-              return response.json();
-            } catch (error) {
-              Swal.showValidationMessage(`Request failed: ${error}`);
-            }
-          },
-          allowOutsideClick: () => !Swal.isLoading()
-        }).then((result) => {
-          if (result.isConfirmed) {
-            Swal.fire({
-              title: `${result.value.login}'s avatar`,
-              imageUrl: result.value.avatar_url
-            });
-          }
-        });
-      });
+//       $('#searchButton').on('click', function() {
+//         Swal.fire({
+//           title: "Bin is Available",
+//           input: "text",
+//           inputAttributes: {
+//             autocapitalize: "on"
+//           },
+//           showCancelButton: true,
+//           confirmButtonText: "Find",
+//           showLoaderOnConfirm: true,
+//           preConfirm: async (login) => {
+//             try {
+//               const githubUrl = `https://odbc.robinsandday.co.uk/ExecuteSql`;
+//               const response = await fetch(githubUrl);
+//               if (!response.ok) {
+//                 return Swal.showValidationMessage(`Request failed: ${response.statusText}`);
+//               }
+//               return response.json();
+//             } catch (error) {
+//               Swal.showValidationMessage(`Request failed: ${error}`);
+//             }
+//           },
+//           allowOutsideClick: () => !Swal.isLoading()
+//         }).then((result) => {
+//           if (result.isConfirmed) {
+//             Swal.fire({
+//               title: `${result.value.login}'s avatar`,
+//               imageUrl: result.value.avatar_url
+//             });
+//           }
+//         });
+//       });
 
-      // Remove the modal from the DOM when it's closed to prevent clutter
-      $('#myModal').on('hidden.bs.modal', function() {
-        $(this).remove();
-      });
+//       // Remove the modal from the DOM when it's closed to prevent clutter
+//       $('#myModal').on('hidden.bs.modal', function() {
+//         $(this).remove();
+//       });
 
-      $('.modal-backdrop').on('hidden.bs.modal', function() {
-        $(this).remove();
-      });
+//       $('.modal-backdrop').on('hidden.bs.modal', function() {
+//         $(this).remove();
+//       });
 
-      console.log("Mouse leave detected");
-    });
-  }).catch((error) => {
-    console.error("Failed to load scripts:", error);
-  });
-});
+//       console.log("Mouse leave detected");
+//     });
+//   }).catch((error) => {
+//     console.error("Failed to load scripts:", error);
+//   });
+// });
 
 
 
