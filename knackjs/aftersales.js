3c536375ef04bb3a43c7934fc6ec09c0e0f08b2e
JasonSLe <107549434+JasonSLe@users.noreply.github.com>
2024-08-28 14:12:32 +0100

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -625,6 +625,19 @@ $(document).on("knack-scene-render.scene_119", function(event, scene, data) {
   //generateTyres();
 });
 
+//pre-visit jobcard tyres to reload stapleton view after populated
+$(document).on("knack-scene-render.scene_1103", function(event, scene, data) {
+    let refreshData = [
+      {
+          mainField : 'field_250', //Tyres
+          views:['3518'],
+          runAfter : generateTyres2 
+      }
+    ]
+    sceneRefresh(refreshData);
+});
+
+
   var recordId = '';
   $(document).on('knack-form-submit.view_71', function(event, view, data) { 
     let commandURL = "https://hook.integromat.com/53yx2tuy820lvzuobdqex8jem2utgwil" ;
