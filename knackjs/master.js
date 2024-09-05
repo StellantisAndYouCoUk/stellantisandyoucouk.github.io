8630bd749e4761328daf5cf907dd1f5fd12ba158
makifyeginS <mehmet.yegin@stellantis.com>
2024-09-05 12:20:02 +0100

    Update master.js
    dev

--- knackjs/master.js
+++ knackjs/master.js
@@ -4916,28 +4916,30 @@ function createLink(url, linkText){
         // });
 
      
-            Swal.fire({
-              title: `<strong>${parsedData.title}</strong>`,
-              icon: "info",
-              html: `
-                You can use <b>bold text</b>,
-                <a href="#" autofocus>links</a>,
-                and other HTML tags
-                <img src='${parsedData.attachment.url}'>
-              `,
-              showCloseButton: true,
-              showCancelButton: true,
-              focusConfirm: false,
-              confirmButtonText: `
-                <i class="fa fa-thumbs-up"></i> Great!
-              `,
-              confirmButtonAriaLabel: "Thumbs up, great!",
-              cancelButtonText: `
-                <i class="fa fa-thumbs-down"></i>
-              `,
-              cancelButtonAriaLabel: "Thumbs down",
-              allowOutsideClick: false
-            });
+        Swal.fire({
+          title: `<strong>${parsedData.title}</strong>`,
+          icon: "info",
+          html: `
+            You can use <b>bold text</b>,
+            <a href="#" autofocus>links</a>,
+            and other HTML tags
+            <br>
+            <img src='${parsedData.attachment.url}' style="max-width: 100%; height: auto;">
+          `,
+          showCloseButton: true,
+          showCancelButton: true,
+          focusConfirm: false,
+          confirmButtonText: `
+            <i class="fa fa-thumbs-up"></i> Great!
+          `,
+          confirmButtonAriaLabel: "Thumbs up, great!",
+          cancelButtonText: `
+            <i class="fa fa-thumbs-down"></i>
+          `,
+          cancelButtonAriaLabel: "Thumbs down",
+          allowOutsideClick: false
+        });
+        
 
 
     }
