99437a1d76e62b388c14afb6a03e935524a7bb5f
JasonSLe <107549434+JasonSLe@users.noreply.github.com>
2024-09-12 15:52:18 +0100

    Update master.js

--- knackjs/master.js
+++ knackjs/master.js
@@ -497,6 +497,13 @@ $(document).on('knack-view-render.view_7326', function(event, view, data) {
   $('div[class*="field_3"]').html('<iframe src="https://www.stellantisandyou.co.uk/digital-orders?token='+encodeURIComponent(token) + ' #customer-follow-up-calls" allow="camera" frameborder="0" width="100%" id="knack-iframe"></iframe>');
 });
 
+//
+//Demo and Courtesy Reporting
+$(document).on('knack-view-render.view_7480', function(event, view, data) {
+  var token = Knack.getUserAttributes().values["field_6440"];
+  $('div[class*="field_3"]').html('<iframe src="https://www.stellantisandyou.co.uk/digital-orders?token='+encodeURIComponent(token) + ' #demo-courtesy-reporting" allow="camera" frameborder="0" width="100%" id="knack-iframe"></iframe>');
+});
+
 var aftersalesConnectView = [{view:'view_6320',url:'#technician-view-my-jobs-v2'},
 //{view:'view_6483',url:'#workshop-control/'},
 			     {view:'view_6510',url:'#aftersales-key-tag-search'},
