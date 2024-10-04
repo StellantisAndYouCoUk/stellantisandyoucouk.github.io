173c271fcaf5fc5183d2a7b7569e4899a7380803
Hynek <hynek@hruska.cz>
2024-10-04 17:21:35 +0200

    dev

--- knackjs/takePhotoApp.js
+++ knackjs/takePhotoApp.js
@@ -366,9 +366,6 @@ function isCtxOnlyBlack(ctxT, width, height){
 for (let i = 1;i<5;i++){
     let p1 = ctxT.getImageData(i*width/7, i*height/7, 1, 1);
     for (let j = 0;j<3;j++){
-      if (Knack.getUserAttributes().email.includes('hynek')){
-        alert(p1.data[j])
-      }
       if (p1.data[j]>10) return false
     }
   }
