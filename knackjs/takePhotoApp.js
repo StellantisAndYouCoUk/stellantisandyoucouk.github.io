941354613ef46d8bc20cbcb581ac74e93126c29f
Hynek <hynek@hruska.cz>
2024-10-04 17:12:23 +0200

    dev

--- knackjs/takePhotoApp.js
+++ knackjs/takePhotoApp.js
@@ -365,8 +365,8 @@ function testBlackScreen(){
 function isCtxOnlyBlack(ctxT, width, height){
 for (let i = 1;i<5;i++){
     let p1 = ctxT.getImageData(i*width/7, i*height/7, 1, 1);
-    for (let j = 0;j<p1.length;j++){
-      if (p1[j]<250) return false
+    for (let j = 0;j<p1.data.length;j++){
+      if (p1.data[j]<250) return false
     }
   }
   return true;
