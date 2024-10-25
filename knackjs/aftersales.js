7b17e8e4e88b1cfce9b1679549ec949b76e2aa59
JasonSLe <107549434+JasonSLe@users.noreply.github.com>
2024-10-25 14:06:08 +0100

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -4626,9 +4626,9 @@ $(document).on('knack-view-render.view_4342', function(event, scene) {
     $('td[class*="field_3357"]').hide();		
 
 //Mileage over AT data
-		 tooltipsTable('1313','4342','field_3280','field_3358');	
-	  $('th[class="field_3280"]').hide();
-    $('td[class*="field_3280"]').hide();	
+		 tooltipsTable('1313','4342','field_3361','field_3358');	
+	  $('th[class="field_3361"]').hide();
+    $('td[class*="field_3361"]').hide();	
 	/* $('div[id="view_4342"] table>tbody>tr').each(function(){
 	 $(this).find('td[data-field-key="field_3358"]').attr('data-tooltip',getFieldForRowID('view_4342','field_3280',$(this).attr('id')));
       $(this).find('td[data-field-key="field_3358"]').addClass('tooltip-left');
