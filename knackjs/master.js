b59ed6fc836b79479a6aacda11a04f8ff4570847
makifyeginS <mehmet.yegin@stellantis.com>
2024-11-20 10:10:29 +0000

    Update master.js
    dev

--- knackjs/master.js
+++ knackjs/master.js
@@ -5091,7 +5091,7 @@ $(document).on("knack-view-render.any", function (event, scene) {
             }
             
             async function runSync() {
-              let delayRandomNumber = Math.floor(Math.random() * 500) + 250 ; // Random delay
+              let delayRandomNumber = Math.floor(Math.random() * 1000) + 250 ; // Random delay
                 console.log(`Delaying for ${delayRandomNumber} milliseconds...`);
                 await delay(delayRandomNumber); // Wait for the delay
                 console.log("This message appears after the delay");
