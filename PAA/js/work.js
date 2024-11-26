3bcfcda722a90991d82000794a125ad91ccec3cc
Hynek <hynek@hruska.cz>
2024-11-26 17:25:53 +0100

    dev

--- PAA/js/work.js
+++ PAA/js/work.js
@@ -1,1 +1,44 @@
-alert('aaa');
+function checkAuth(){
+    if (window.location.href.includes('login.htlm')) return;
+    let paaToken = readCookie('paaToken');
+    if (!paaToken){
+        window.location = './login.html'
+    }
+}
+
+function createCookie(name, value, days) {
+    var expires;
+
+    if (days) {
+        var date = new Date();
+        date.setDate(date.getDate() + days); 
+        date.setHours(0,0,0,0);
+        expires = "; expires=" + date.toGMTString();
+    } else {
+        expires = "";
+    }
+    document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + expires + "; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = encodeURIComponent(name) + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ')
+            c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0)
+            return decodeURIComponent(c.substring(nameEQ.length, c.length));
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name, "", -1);
+}
+
+checkAuth();
+
+$("a[id='loginButton']").bind( "click", function() {
+    alert( "User clicked on 'foo.'" );
+});
