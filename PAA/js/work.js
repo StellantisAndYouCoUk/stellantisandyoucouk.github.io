3b43560dd9e86b30f52982bd3d5e0e6503405725
Hynek <hynek@hruska.cz>
2024-11-28 00:18:37 +0100

    dev

--- PAA/js/work.js
+++ PAA/js/work.js
@@ -125,7 +125,7 @@ function work(){
     if (page.includes('flows.html')){
         let req = paaPostRequest({'action':'getFlows','token':paaToken});
         let tM = req.map(function (el){
-            return '<tr><td>'+el.name+'</td><td>'+el.inputs+'</td></tr>';
+            return '<tr><td>'+el.name+'</td><td>'+(el.integrations?el.integrations.length:'')+'</td><td>'+el.inputs+'</td></tr>';
         })
         $('table[id="datatablesSimpleFlows"]>tbody').append(tM.join(''));
         const datatablesSimple = document.getElementById('datatablesSimpleFlows');
