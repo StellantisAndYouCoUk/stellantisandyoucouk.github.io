1250472f5b6e44ae5597b593959270d965149da9
Hynek <hynek@hruska.cz>
2024-12-06 09:54:46 +0100

    dev

--- knackjs/orders.js
+++ knackjs/orders.js
@@ -149,7 +149,6 @@ var submitUserLoginForm = function() {
   let token = getTokenFromURL(url);
   console.log('token',token);
   if (token) token = atob(token);
-  token = atob(token);
   if (!token){
     if ($('[id="email"]').length>0 && $('[id="password"]').length>0){
       console.log('on page direct without login');
