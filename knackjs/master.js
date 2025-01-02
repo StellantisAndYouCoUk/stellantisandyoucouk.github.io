d46e4f282c94a84a624360856e60a50050f45a15
makifyeginS <mehmet.yegin@stellantis.com>
2025-01-02 16:03:29 +0000

    Update master.js
    dev

--- knackjs/master.js
+++ knackjs/master.js
@@ -5540,58 +5540,7 @@ $(document).on('knack-scene-render.any', function(event, scene) {
   
 });
 
-$(document).on('knack-scene-render.any', function(event, scene) {
 
-  console.log("Knack scene render any Test for cors");
-
-  // $.ajax({
-  //   url: 'https://stellantisandyoucouk.github.io/modalHTML/user.html', // Replace with your actual URL
-  //   type: 'GET',
-  //   success: function (data) {
-  //       // Append fetched content while preserving existing .user-icon
-  //       $('.user').append(`<div class="dropdown-content">${data}</div>`);
-  //   },
-  //   error: function () {
-  //       alert('Failed to load dropdown content');
-  //   }
-  // });
-
-  // Click event on user icon
-  // $('.user-icon').on('click', function (event) {
-  //    console.log("Clicked to load function")
-      // event.stopPropagation(); // Prevent the click from bubbling to the document
-
-      // Check if dropdown content is already loaded
-      // if ($('.user .dropdown-content').length === 0) {
-      //     // Load dropdown content via AJAX only if it's not already loaded
-          // $.ajax({
-          //     url: 'https://stellantisandyoucouk.github.io/modalHTML/user.html', // Replace with your actual URL
-          //     type: 'GET',
-          //     success: function (data) {
-          //         // Append fetched content while preserving existing .user-icon
-          //         $('.user').append(`<div class="dropdown-content">${data}</div>`);
-          //     },
-          //     error: function () {
-          //         alert('Failed to load dropdown content');
-          //     }
-          // });
-      // } else {
-      //     // Toggle visibility of the dropdown content
-      //     $('.user .dropdown-content').toggle();
-      // }
-  // });
-
-  // Close dropdown when clicking outside
-  // $(document).on('click', function () {
-  //     $('.user .dropdown-content').hide(); // Hide only the dropdown content
-  // });
-
-  // Prevent click inside dropdown from closing it
-  // $('.user').on('click', function (event) {
-  //     event.stopPropagation(); // Prevent the click from propagating to the document
-  // });
-
-});
 
 $(document).on('knack-scene-render.scene_436', function(event, scene) {
   console.log('knack-scene-render.scene_436');
