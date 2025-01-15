b11348209b40b75a2001ca2aaaaca8c542a75a66
Lizziebolton123 <149383954+Lizziebolton123@users.noreply.github.com>
2025-01-15 10:31:43 +0000

    Update master.js

--- knackjs/master.js
+++ knackjs/master.js
@@ -5585,6 +5585,8 @@ $(document).on('knack-scene-render.any', function(event, scene) {
 });
 
 
+// Hide card reader file name in Deposit table
+
 $(document).on('knack-records-render.view_7588', function (event, scene, records) {
     $("#view_7588").find("td.field_10279").each(function () {
         if ($(this).text().trim() !== "") {
