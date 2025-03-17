d500d67ef3345072458eedc23bf01266b136ae04
makifyeginS <mehmet.yegin@stellantis.com>
2025-03-17 22:03:38 +0000

    Update master.js
    dev

--- knackjs/master.js
+++ knackjs/master.js
@@ -5381,7 +5381,7 @@ $(document).on("knack-view-render.any", function (event, scene) {
         function showNotificationBackground(title, icon = '', body) {   
 
           if('serviceWorker' in navigator){
-            navigator.serviceWorker.register('sw.js')
+            navigator.serviceWorker.register('./sw.js')
             .then((reg)=>{console.log('service worker registered', reg)})
             .catch((err)=>{console.log('service worker not registered', err)})
           }
