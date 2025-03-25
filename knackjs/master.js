7eebc6eca7278e4603bca2ef2068c94bebaf60c5
makifyeginS <mehmet.yegin@stellantis.com>
2025-03-25 14:17:01 +0000

    Update master.js
    dev

--- knackjs/master.js
+++ knackjs/master.js
@@ -5238,7 +5238,7 @@ $(document).on("knack-view-render.any", function (event, scene) {
  
 
 
-        if (Notification.permission === 'granted'){
+        if (Notification.permission === 'granted' || message.ShowMessageDirectly){
           if(!message.Title.toString().includes("Message") && !message.Title.toString().includes("Waiting Sales") || message.InAppPopUp){
 
             
@@ -5306,7 +5306,7 @@ $(document).on("knack-view-render.any", function (event, scene) {
               },
             });
           }
-        }else{
+        }else {
             console.log("Missing Notification");
 
 
