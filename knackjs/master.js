9f27c7770206cee41042bc445e41ef027bfbd850
makifyeginS <mehmet.yegin@stellantis.com>
2025-04-04 00:26:43 +0100

    Update master.js
    dev

--- knackjs/master.js
+++ knackjs/master.js
@@ -5860,29 +5860,32 @@ $(document).on('knack-scene-render.any', function(event, scene) {
                   "background-color": "hsl(0deg 0% 92.16%)",
                   "border": "unset"
                 });
+
+
+                setTimeout(() => {
+
+
+
+                  $(document).on('knack-scene-render.scene_2335', function(event, scene) {
+                    console.log("Scene_2335 running");
+                    console.log("Firework")
+                    LazyLoad.js(['https://cdn.jsdelivr.net/npm/@tsparticles/confetti@3.0.3/tsparticles.confetti.bundle.min.js'], function () {
+                      confetti()
+                    
+                  });
+          
+                    })
+          
+          
+          
+                  
+                }, 3000);
+
                 
               }
             };
                   updateNotificationUI();
 
-                  setTimeout(() => {
-
-
-
-                    $(document).on('knack-scene-render.scene_2335', function(event, scene) {
-                      console.log("Scene_2335 running");
-                      console.log("Firework")
-                      LazyLoad.js(['https://cdn.jsdelivr.net/npm/@tsparticles/confetti@3.0.3/tsparticles.confetti.bundle.min.js'], function () {
-                        confetti()
-                      
-                    });
-            
-                      })
-            
-            
-            
-                    
-                  }, 3000);
 
 
 
