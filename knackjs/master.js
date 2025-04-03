6fbaa118dfc9e8300422c853639c85f91e021bff
makifyeginS <mehmet.yegin@stellantis.com>
2025-04-03 19:06:40 +0100

    Update master.js
    dev

--- knackjs/master.js
+++ knackjs/master.js
@@ -5869,8 +5869,10 @@ $(document).on('knack-scene-render.any', function(event, scene) {
               console.log("isTablet: " + isTablet)
               console.log("isPhone: " + isPhone)
 
-          
+              var user = Knack.getUserToken();
+             
                 if (
+                    user &&
                     Notification.permission === 'denied' &&
                     !isTablet &&
                     !isPhone &&
@@ -5878,9 +5880,9 @@ $(document).on('knack-scene-render.any', function(event, scene) {
                 ) {
                     console.log("Redirect");
                     localStorage.setItem("previousPage", window.location.href);
-                    // window.setTimeout(function() {
-                    //     window.location.href = 'https://www.stellantisandyou.co.uk/digital#account-settings/enable-desktop-notification/';
-                    // }, 500);
+                    window.setTimeout(function() {
+                        window.location.href = 'https://www.stellantisandyou.co.uk/digital#account-settings/enable-desktop-notification/';
+                    }, 500);
                 }
      
 
