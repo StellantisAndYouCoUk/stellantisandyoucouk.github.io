16b896bea90d5e65093dc838a44170b024b19017
makifyeginS <mehmet.yegin@stellantis.com>
2025-04-24 17:47:56 +0100

    dev
    dev

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -4268,15 +4268,3 @@ $(document).on('knack-view-render.view_738', function (event, view, data) {
 	  $('th[class="field_2189"]').hide();
     $('td[class*="field_2189"]').hide();
 }); 
-
-
-
-$(document).on('knack-scene-render.scene_2324', function (event, scene, data) {
-  //This part is for tooltip of another field above field in list
-  //This part of code hides field_330 from the list and then adds it as mouse over to field 380
-  //It needs function "getFieldForRowID", also the field_330 NEEDS to be included in the list
-  // tooltipsTable('540','1880','field_318','field_763');
-  // tooltipsTable('540','1880','field_1537','field_899');
-
-  console.log("2324 running")
-}); 
