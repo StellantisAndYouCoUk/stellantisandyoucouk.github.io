eb5c8e02fdc808bef57226f8f19df98106567a4d
makifyeginS <137415127+makifyeginS@users.noreply.github.com>
2025-07-04 15:08:54 +0100

    Update master.js

--- knackjs/master.js
+++ knackjs/master.js
@@ -5993,9 +5993,9 @@ $(document).on('knack-scene-render.any', function(event, scene) {
 
 
                   
-                    window.setTimeout(function() {
-                        window.location.href = 'https://www.stellantisandyou.co.uk/digital#account-settings/enable-desktop-notification/';
-                    }, 500);
+                    // window.setTimeout(function() {
+                    //     window.location.href = 'https://www.stellantisandyou.co.uk/digital#account-settings/enable-desktop-notification/';
+                    // }, 500);
                 }
      
 
