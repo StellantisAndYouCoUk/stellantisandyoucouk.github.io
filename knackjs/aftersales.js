4189c8736bfe3d7b81891a73944714fec9d53730
makifyeginS <137415127+makifyeginS@users.noreply.github.com>
2025-08-07 14:14:04 +0100

    Update aftersales.js

--- knackjs/aftersales.js
+++ knackjs/aftersales.js
@@ -4724,7 +4724,7 @@ $(document).on('knack-scene-render.scene_1480', function(event, scene) {
                             };
                             if (currentRow.id!==''){
                                 console.log(currentRow.id);
-                                 console.log(typeof(currentRow));
+                                console.log(currentRow.children[20]);
                               currentRow.children[20].onclick = createClickHandler(currentRow);
                             }
                           }
