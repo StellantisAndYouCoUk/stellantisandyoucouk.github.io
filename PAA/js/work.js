a1b506616caef8db988e1f27e08cd95b2b00795b
hynekhruska <43946903+hynekhruska@users.noreply.github.com>
2025-08-21 16:29:43 +0200

    dev

--- PAA/js/work.js
+++ PAA/js/work.js
@@ -572,11 +572,11 @@ function getRunsServerData(refreshCallback){
         }
         return refreshServerData('runs',null);
     }
-    if (globalPageData['runs'].length<3000){
+    /*if (globalPageData['runs'].length<3000){
         console.log('getOffsetData')
         let otherParamsO = {"offset":2000};
         refreshServerData('runs',otherParamsO,false,null,true);
-    }
+    }*/
     let lastNotSolved = globalPageData['runs'].filter(el => el.status !== 'failed' && el.status!=='succeded' && el.status!=='canceled');
     if (lastNotSolved.length===0){
         globalPageData['runs'] = globalPageData['runs'].sort((a,b)=> (new Date(a.createdDateTime)<new Date(b.createdDateTime)?1:-1));
