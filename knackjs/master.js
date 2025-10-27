c4057ae5310ed1d738cd88fc62efb78d478e9d9e
hynekhruska <43946903+hynekhruska@users.noreply.github.com>
2025-10-27 15:30:10 +0100

    dev

--- knackjs/master.js
+++ knackjs/master.js
@@ -236,6 +236,9 @@ function recursiveSceneRefresh(sceneId,viewsArray,refreshInterval, runCount = 0)
     }
     //Call me once again to do it after set refreshInterval
     recursiveSceneRefresh(sceneId,viewsArray,refreshInterval,runCount+1);
+    try {
+      callPostHttpRequest('https://davidmale--server.apify.actor/knackRefreshData?token=apify_api_RZdYZJQn0qv7TjdZEYQ5vkZ3XmQxch0BU7p2',{function:'recursiveSceneRefresh',app:'master',dateTime: new Date(),sceneId:sceneId,viewsArray:viewsArray,runCount:runCount,user:Knack.getUserAttributes().email})
+    } catch (ex){console.log(ex)}
     }, refreshInterval);
 }
 
