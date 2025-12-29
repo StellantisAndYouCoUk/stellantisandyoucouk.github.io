4954a1289d38aff1a7677e4c0873ce2e41c03b1f
hynekhruska <43946903+hynekhruska@users.noreply.github.com>
2025-12-29 17:12:22 +0100

    dev

--- serviceBooking/js/work.js
+++ serviceBooking/js/work.js
@@ -278,6 +278,9 @@ function work(){
             if (serviceBookingProcess.bookingData && serviceBookingProcess.bookingData.pricing){
                 $('div[id="step3"]').show(); 
                 generatePricingHTML();
+                $("input[name='otherCode']").bind("click", function() {
+                    console.log('fff',$(this));
+                });
                 generateBookingSummary();
             }
         }
@@ -285,7 +288,7 @@ function work(){
 }
 
 function generateBookingSummary(){
-    let html = serviceBookingProcess.bookingData.dealerName+'<br/>'+serviceBookingProcess.bookingData.bookingVehicleDescription+' '+serviceBookingProcess.bookingData.mileage;
+    let html = serviceBookingProcess.bookingData.dealerName+'<br/>'+serviceBookingProcess.bookingData.bookingVehicleDescription+' - '+serviceBookingProcess.bookingData.mileage+' miles';
     $('div[id="bookingSummary"]').html(html);
 }
 
@@ -302,7 +305,7 @@ function generatePricingHTML(){
 function generateTableFromData(data, isServiceSchedule = false){
     let html = '<table class="table table-condensed" width="100%"><tbody>';
     for (let i = 0;i<data.length;i++){
-        html += '<tr'+(data[i].IsPreselected?' style="background-color: yellow;"':'')+'><td><input type="checkbox" class="ng-pristine ng-untouched ng-valid ng-empty"></td>'+(isServiceSchedule?'<td class="ng-binding">Year '+data[i].Age+'</td><td class="ng-binding">'+data[i].Mileage+'</td>':'')+'<td class="ng-binding">'+data[i].Code+'</td><td class="ng-binding">'+data[i].Name+'</td><td style="text-align: right;" class="ng-binding">'+data[i].PriceDisplay+'</td></tr>'
+        html += '<tr'+(data[i].IsPreselected?' style="background-color: yellow;"':'')+'><td><input type="checkbox" name="'+(isServiceSchedule?'serviceScheduleCode':'otherCode')+'" class="ng-pristine ng-untouched ng-valid ng-empty"></td>'+(isServiceSchedule?'<td class="ng-binding">Year '+data[i].Age+'</td><td class="ng-binding">'+data[i].Mileage+'</td>':'')+'<td class="ng-binding">'+data[i].Code+'</td><td class="ng-binding">'+data[i].Name+'</td><td style="text-align: right;" class="ng-binding">'+data[i].PriceDisplay+'</td></tr>'
     }
     html += '</tbody></table>';
     return html;
