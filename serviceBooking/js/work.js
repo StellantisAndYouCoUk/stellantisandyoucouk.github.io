3599a67e0bba5c9e623c2353eedb4d00746704a2
hynekhruska <43946903+hynekhruska@users.noreply.github.com>
2025-12-30 15:00:52 +0100

    dev

--- serviceBooking/js/work.js
+++ serviceBooking/js/work.js
@@ -373,7 +373,7 @@ function refreshAvailabilityData(companyCode, hardRefresh = false){
 
 function findAvailabilityDaysForBooking(){
     if (serviceBookingProcess.bookingData.labourSummary){
-        let av = callPostHttpRequest('https://davidmale--shared-server-1.apify.actor/getWorkshopAvailabilityForLabour?token=apify_api_pt5m4fzVRYCWBTCdu5CKzc02hKZkXg2eeqW3',null,{token:token,companyCode:companyCode,labourArray:labourSummary});
+        let av = callPostHttpRequest('https://davidmale--shared-server-1.apify.actor/getWorkshopAvailabilityForLabour?token=apify_api_pt5m4fzVRYCWBTCdu5CKzc02hKZkXg2eeqW3',null,{token:token,companyCode:serviceBookingProcess.bookingData.dealer.field_2442,labourArray:labourSummary});
         console.log(av);
     }
 }
