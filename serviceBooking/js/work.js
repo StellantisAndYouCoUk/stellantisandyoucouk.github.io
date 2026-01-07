4fc5b0e218578ef0a65f9d2f1b82060d7c8a989f
hynekhruska <43946903+hynekhruska@users.noreply.github.com>
2026-01-07 17:03:30 +0100

    dev

--- serviceBooking/js/work.js
+++ serviceBooking/js/work.js
@@ -380,6 +380,7 @@ function getVehicleDescription(){
         out = out + "The current owner bought this " + (dateOfCurrentUserBought.getFullYear().toString() === serviceBookingProcess.dvlaData.monthOfFirstRegistration.substring(0,4) & pad(dateOfCurrentUserBought.getMonth()+1) === serviceBookingProcess.dvlaData.monthOfFirstRegistration.substring(5,7)?"NEW":"USED") + " vehicle <b>" + (new Date() - dateOfCurrentUserBought)/(1000*60*60*24*365).toFixed(1) + "</b> years ago" //+ if(parseDate(82.data.dateOfLastV5CIssued; "YYYY-MM-DD") > 84.dateOne | parseDate(82.data.dateOfLastV5CIssued; "YYYY-MM-DD") > 84.dateTwo; ". We cannot assess mileage because of owner change."; if((84.dateOne - 84.dateTwo) > 0; " and travels around <b>" + formatNumber((84.mileageOne - 84.mileageTwo) / ((84.dateOne - 84.dateTwo) / 1000 / 60 / 60 / 24); 0) + "</b> miles a day and <b>" + formatNumber((365 * (84.mileageOne - 84.mileageTwo) / ((84.dateOne - 84.dateTwo) / 1000 / 60 / 60 / 24)); 0; emptystring; emptystring) + "</b> miles a year. (" + 84.basedOn + ")"; ".")))}}
     }
     return out;
+    
 }
 
 function getCustomerDetails(){
