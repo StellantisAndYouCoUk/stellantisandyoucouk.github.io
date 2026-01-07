8ee55d22ef7eff01e0ae35deeaa14c4bf109432f
hynekhruska <43946903+hynekhruska@users.noreply.github.com>
2026-01-07 17:01:11 +0100

    dev

--- serviceBooking/js/work.js
+++ serviceBooking/js/work.js
@@ -226,12 +226,24 @@ function searchRegistration(registrationNumber){
         $('div[id="searchRegistrationMessage"]').hide();
         Object.assign(serviceBookingProcess, r.data)
         sessionStorage.setItem('serviceBookingProcess',JSON.stringify(serviceBookingProcess));
+        setTimeout(() => {
+            getSecondaryDetails(registrationNumber,(r.data.customer?r.data.customer.CustomerNumber:null),(r.data.vehicle?r.data.vehicle.VehicleNumber:null))
+        }, 100);
         work();
     } else {
         $('div[id="searchRegistrationMessage"]').text('Vehicle was not found in Autoline and not found in DVLA register');
         $('div[id="searchRegistrationMessage"]').show();
     }
     $("a[id='searchRegistration']").prop("disabled", false);
+}
+
+function getSecondaryDetails(registrationNumber, customerNumber=null,vehicleNumber=null){
+    let r = callPostHttpRequest('https://davidmale--shared-server-1.apify.actor/getDetailsSecondary?token=apify_api_pt5m4fzVRYCWBTCdu5CKzc02hKZkXg2eeqW3',null,{token:token,registrationNumber:registrationNumber,customerNumber:customerNumber,vehicleNumber:vehicleNumber});
+    if (r.success && r.data){
+        console.log('getSecondaryDetails success')
+        Object.assign(serviceBookingProcess,r.data);
+        work();
+    }
 }
 
 function newVehicle(){
