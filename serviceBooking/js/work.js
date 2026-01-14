e52524981b52ed50d09cd5ba0341fd0752ee11c0
hynekhruska <43946903+hynekhruska@users.noreply.github.com>
2026-01-14 13:39:08 +0100

    dev

--- serviceBooking/js/work.js
+++ serviceBooking/js/work.js
@@ -428,7 +428,7 @@ function getVehicleDescription(){
     }
     if (serviceBookingProcess.secondaryDetails && serviceBookingProcess.secondaryDetails.autotraderData && serviceBookingProcess.secondaryDetails.autotraderData.vehicle && serviceBookingProcess.secondaryDetails.autotraderData.vehicle.grossVehicleWeightKG){
         out += '<br />Gross Weight: '+serviceBookingProcess.secondaryDetails.autotraderData.vehicle.grossVehicleWeightKG+' Kg';
-        out += (serviceBookingProcess.secondaryDetails.autotraderData.vehicle.grossVehicleWeightKG >= 3500?(serviceBookingProcess.secondaryDetails.autotraderData.vehicle.bodyType.includes("Window")?"<p style=\"color:red\"><b><i>CLASS 5</b></i></p>":"<p style=\"color:red\"><b><i>CLASS 7</b></i></p>"):"<p style=\"color:red\"><b><i>CLASS 4</b></i></p>");
+        out += (serviceBookingProcess.secondaryDetails.autotraderData.vehicle.grossVehicleWeightKG >= 3500?(serviceBookingProcess.secondaryDetails.autotraderData.vehicle.bodyType.includes("Window")?"<span style=\"color:red\"><b><i>CLASS 5</b></i></span>":"<span style=\"color:red\"><b><i>CLASS 7</b></i></span>"):"<span style=\"color:red\"><b><i>CLASS 4</b></i></span>");
     }
 
     if (!serviceBookingProcess.dvlaData){
