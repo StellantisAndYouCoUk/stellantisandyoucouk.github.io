0f2426f2612bbea263c9e1c4edc234a1b3566304
hynekhruska <43946903+hynekhruska@users.noreply.github.com>
2026-01-23 14:19:46 +0100

    dev

--- serviceBooking/js/work.js
+++ serviceBooking/js/work.js
@@ -857,7 +857,7 @@ async function generateBookingSummary(){
     $('div[id="bookingSummary"]').html(html);
     if (serviceBookingProcess.bookingData.confirmAvailability){
         if (serviceBookingProcess.bookingData.confirmAvailability.status==='checking'){
-            html += '<br /><b><img src="https://stellantisandyoucouk.github.io/imagesStore/loading.gif"> <span style=\"color:yellow;\">Checking availability for date '+ dateToGB(serviceBookingProcess.bookingData.confirmAvailability.date)+'</span></b>'
+            html += '<br /><b><img src="https://stellantisandyoucouk.github.io/imagesStore/loading.gif"> <span style=\"color:orange;\">Checking availability for date '+ dateToGB(serviceBookingProcess.bookingData.confirmAvailability.date)+'</span></b>'
         } else {
             html += '<br />'+(serviceBookingProcess.bookingData.confirmAvailability.dateAvailable?'<span style=\"color:green;\">':'<span style=\"color:red;\">')+'<b>Date '+ dateToGB(new Date(serviceBookingProcess.bookingData.confirmAvailability.date)) + ' ' + (serviceBookingProcess.bookingData.confirmAvailability.dateAvailable?'available':'NOT AVAILABLE')+'</b></span>'
         }
