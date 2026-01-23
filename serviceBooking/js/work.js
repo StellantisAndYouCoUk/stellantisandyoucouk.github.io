f15fb867bd1bbc491b519bbd2fea7dc2b3cac987
hynekhruska <43946903+hynekhruska@users.noreply.github.com>
2026-01-23 17:01:31 +0100

    dev

--- serviceBooking/js/work.js
+++ serviceBooking/js/work.js
@@ -847,7 +847,7 @@ async function generateBookingSummary(){
         for (let i = 0;i<serviceBookingProcess.bookingData.orderedCodes.length;i++){
             let justCode = serviceBookingProcess.bookingData.orderedCodes[i].split('#')[1];
             //console.log(justCode,serviceBookingProcess.bookingData.orderedCodes[i].split('#')[0]);
-            let pricingDetailsForCode = (serviceBookingProcess.bookingData.orderedCodes[i].split('#')[0].includes('serviceSchedule_')?serviceBookingProcess.bookingData.pricing.ServiceSchedule.ServiceIntervals.find(el => el.Code === justCode):serviceBookingProcess.bookingData.pricing[serviceBookingProcess.bookingData.orderedCodes[i].split('#')[0]].find(el => el.Code === justCode));
+            let pricingDetailsForCode = (serviceBookingProcess.bookingData.orderedCodes[i].split('#')[0].includes('serviceSchedule_')?serviceBookingProcess.bookingData.pricing.ServiceSchedule && serviceBookingProcess.bookingData.pricing.ServiceSchedule.ServiceIntervals.find(el => el.Code === justCode):serviceBookingProcess.bookingData.pricing[serviceBookingProcess.bookingData.orderedCodes[i].split('#')[0]].find(el => el.Code === justCode));
             if (!pricingDetailsForCode){
                 //We should inform the user
                 excludedCodes.push(serviceBookingProcess.bookingData.orderedCodes[i])
