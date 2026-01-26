6294b0986836b7a275f34597a10899697c1637f6
hynekhruska <43946903+hynekhruska@users.noreply.github.com>
2026-01-26 18:11:52 +0100

    dev

--- serviceBooking/js/work.js
+++ serviceBooking/js/work.js
@@ -452,6 +452,9 @@ function checkBookingDataForDealership(checkDealership){
                     } else {
                         let fT = supportData.konnectFuelTypes.find(el => el.Name.toLowerCase().startsWith(serviceBookingProcess.motData.fuelType.toLowerCase()));
                         if (!fT){
+                            fT = supportData.konnectFuelTypes.find(el => serviceBookingProcess.motData.fuelType.toLowerCase().includes(el.Name.toLowerCase()));
+                        }
+                        if (!fT){
                             $('div[id="bookingProblems"]').text('Fuel type not found in pricing data for '+checkDealership.field_8+', car fuel type: '+serviceBookingProcess.motData.fuelType)
                             $('div[id="bookingProblems"]').show();
                             serviceBookingProcess.bookingData = null;
