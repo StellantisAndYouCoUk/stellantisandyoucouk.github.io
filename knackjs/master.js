2c065e8f2d8b1892aa738fcafc26cdeb929c655d
Lizziebolton123 <149383954+Lizziebolton123@users.noreply.github.com>
2026-01-26 11:34:25 +0000

    Update master.js

--- knackjs/master.js
+++ knackjs/master.js
@@ -606,6 +606,12 @@ $(document).on('knack-view-render.view_8010', function(event, view, data) {
   $('div[class*="field_3"]').html('<iframe src="https://www.stellantisandyou.co.uk/aftersales#spoke-coordinator-visit-forms?token='+encodeURIComponent(token) + '" allow="camera" frameborder="0" width="100%" id="knack-iframe"></iframe>');
 });
 
+// Parts Messaging
+$(document).on('knack-view-render.view_8792', function(event, view, data) {
+  var token = Knack.getUserAttributes().values["field_6440"];
+  $('div[class*="field_3"]').html('<iframe src="https://www.stellantisandyou.co.uk/aftersales#parts-one-way-customer-messaging?token='+encodeURIComponent(token) + '" allow="camera" frameborder="0" width="100%" id="knack-iframe"></iframe>');
+});
+
 //Tyre Stock Auditing
 $(document).on('knack-view-render.view_8101', function(event, view, data) {
   var token = Knack.getUserAttributes().values["field_6440"];
