49d328be9fb1cc0ce2fbc80f666cb5f5f0f03513
DanielCheng1 <118259318+DanielCheng1@users.noreply.github.com>
2026-02-10 09:21:11 +0000

    Update master.js

--- knackjs/master.js
+++ knackjs/master.js
@@ -690,6 +690,12 @@ $(document).on('knack-view-render.view_8527', function(event, view, data) {
   $('div[class*="field_3"]').html('<iframe src="https://www.stellantisandyou.co.uk/digital-orders?token='+encodeURIComponent(token) + '#manage-fleet-customer" allow="camera" frameborder="0" width="100%" id="knack-iframe"></iframe>');
 });
 
+// Fleet Customer Portal
+$(document).on('knack-view-render.view_8850', function(event, view, data) {
+  var token = Knack.getUserAttributes().values["field_6440"];
+  $('div[class*="field_3"]').html('<iframe src="https://www.stellantisandyou.co.uk/digital-orders?token='+encodeURIComponent(token) + '#manage-fleet-customer" allow="camera" frameborder="0" width="100%" id="knack-iframe"></iframe>');
+});
+
 var aftersalesConnectView = [{view:'view_6320',url:'#technician-view-my-jobs-v2'},
 //{view:'view_6483',url:'#workshop-control/'},
 			     {view:'view_6510',url:'#aftersales-key-tag-search'},
