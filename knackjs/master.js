4fc63d59dd8002bb342fa7624ebbc27aa0989480
Lizziebolton123 <149383954+Lizziebolton123@users.noreply.github.com>
2026-02-24 10:04:19 +0000

    Update master.js

--- knackjs/master.js
+++ knackjs/master.js
@@ -577,6 +577,12 @@ $(document).on('knack-view-render.view_7517', function(event, view, data) {
  $('div[class*="field_3"]').html('<iframe src="https://www.stellantisandyou.co.uk/aftersales#service-sales-prospect/?token='+encodeURIComponent(token) + '" allow="camera" frameborder="0" width="100%" id="knack-iframe"></iframe>');
 });
 
+//View of all Aftersales Messages
+$(document).on('knack-view-render.view_8902', function(event, view, data) {
+  var token = Knack.getUserAttributes().values["field_6440"];
+ $('div[class*="field_3"]').html('<iframe src="https://www.stellantisandyou.co.uk/aftersales#view-of-aftersales-messages/?token='+encodeURIComponent(token) + '" allow="camera" frameborder="0" width="100%" id="knack-iframe"></iframe>');
+});
+
 //Overdue Debts
 $(document).on('knack-view-render.view_7578', function(event, view, data) {
   var token = Knack.getUserAttributes().values["field_6440"];
