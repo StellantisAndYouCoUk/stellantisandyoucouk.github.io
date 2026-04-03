f5900ab5edafb5d7f554da5848067fb7f703defc
hynekhruska <43946903+hynekhruska@users.noreply.github.com>
2026-04-03 16:03:28 +0200

    dev

--- PAA/js/work.js
+++ PAA/js/work.js
@@ -443,10 +443,9 @@ function work(){
             refereshRunsTable();
         }
         
-        let isSomethingActive = globalPageData.runs.find(el => el.status!=='succeded' && el.status !=='failed' && el.status !=='canceled');
         setTimeout(() => {
             work();
-        }, (isSomethingActive?15000:45000));
+        }, 15000);
     }
 
     if (page.includes('uicoll.html')){
