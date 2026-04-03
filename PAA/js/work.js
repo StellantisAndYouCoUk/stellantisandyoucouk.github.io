9dff867537ec24088a82d7f8ada09ca229bb87a1
hynekhruska <43946903+hynekhruska@users.noreply.github.com>
2026-04-03 14:25:54 +0200

    dev

--- PAA/js/work.js
+++ PAA/js/work.js
@@ -415,13 +415,13 @@ function work(){
                 },
                 columns: [
                 { data: 'flowName',title: 'Flow Name'},
-                //{ data: 'LiveOrPreprod',title: 'Live'},
+                { data: 'flowInput.liveOrPreprod',title: 'Live'},
                 //{ data: 'Machine',title: 'Machine'},
                 { data: 'runMode',title: 'Mode'},
-                { data: 'status',title: 'State'},
+                { data: 'status',title: 'State', "render": function(data, type) { return (data?data:'queued');}},
                 { data: 'priority',title: 'Pri'},
-                //{ data: 'Requested',title: 'Requested',name:'Requested', "render": function(data, type) { return type === 'sort' ? data : dateTimeToGBNoYear(new Date(data));}},
-                { data: 'startedDateTime',title: 'Started'},
+                { data: 'createdDateTime',title: 'Requested',name:'Requested', "render": function(data, type) { return type === 'sort' ? dateTimeToGB(new Date(data)) : dateTimeToGBNoYear(new Date(data));}},
+                { data: 'startedDateTime',title: 'Started', "render": function(data, type) { return type === 'sort' ? dateTimeToGB(new Date(data)) : dateTimeToGBNoYear(new Date(data));}},
                 //{ data: 'Duration',title: 'Duration'},
                 //{ data: 'Details',title:'Details' },
                 //{ data: 'In PA',title: 'In PA'}
