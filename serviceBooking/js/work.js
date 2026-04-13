1bf3382108fbf693b594300dcb93105486b4fb62
hynekhruska <43946903+hynekhruska@users.noreply.github.com>
2026-04-13 19:35:21 +0200

    dev

--- serviceBooking/js/work.js
+++ serviceBooking/js/work.js
@@ -439,6 +439,7 @@ function work(){
     if (page.includes('index.html')){
         if (qV.regNumber && (!serviceBookingProcess || !serviceBookingProcess.registrationNumber || serviceBookingProcess.registrationNumber.toLowerCase() !== qV.regNumber.toLowerCase())){
             searchRegistration(qV.regNumber);
+            window.history.pushState('index.html', 'Service Booking', '/index.html');
             return;
         }
         if (!serviceBookingProcess.registrationNumber || !serviceBookingProcess.dvlaData){
