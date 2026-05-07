2646f84014e39f5043d081092f671502c83d3186
JasonSLe <107549434+JasonSLe@users.noreply.github.com>
2026-05-07 09:31:13 +0100

    Update master.js

--- knackjs/master.js
+++ knackjs/master.js
@@ -6669,3 +6669,21 @@ function sceneRefresh(refreshData, startTime = null, runCounter = 1, stats = nul
       console.log('sceneRefresh fail', refreshData, e)
     }
 }
+
+
+  //Refresh Recall status for Used Stock
+});$(document).on('knack-scene-render.scene_2882', function(event, scene) {
+ //fillLoading('9187')
+  refreshScene2882();
+});
+
+ function refreshScene2882(){
+  let refreshData = [
+    {
+        name : 'Recall status and Details',
+        mainField : 'field_11952', 
+        views:['9187']   
+    }
+  ]
+  sceneRefresh(refreshData,null,1,null,false);
+}
