c05f473ade995122e1d1a4b16f26dc616e7b1adc
Lizziebolton123 <149383954+Lizziebolton123@users.noreply.github.com>
2026-07-03 13:52:30 +0100

    Update master.js

--- knackjs/master.js
+++ knackjs/master.js
@@ -6943,7 +6943,9 @@ $(document).on('knack-view-render.view_9273', function(event, view, data) {
     const iframe2 = $('<iframe>', {
       src: pdfLink2 + '#navpanes=0',
       frameborder: 0,
-	  width: "100%"
+      width: "100%",
+	  height: "100%",
+	  border: "none"
     });
 
     pdfPane.append(iframe);
