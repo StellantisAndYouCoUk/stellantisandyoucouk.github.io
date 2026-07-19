1361b7c48c1377db82cecc76b6a496aa7eda0b8e
hynekhruska <43946903+hynekhruska@users.noreply.github.com>
2026-07-19 12:35:16 +0200

    dev

--- serviceBooking/js/work.js
+++ serviceBooking/js/work.js
@@ -695,6 +695,8 @@ function getVehicleDescription(){
         out += '<b>Vehicle was not found in Autoline</b><br /><br />'
         if (serviceBookingProcess.customer){
             out += '<button onlick="createVehicleInAutoline(); return false;">Create vehicle in Autoline connected to the customer</button><br /><br />'
+        } else {
+            out += 'To create the vehicle in Autoline choose or create customer first<br /><br />'
         }
     }
     if ((serviceBookingProcess.motData.firstUsedDate && serviceBookingProcess.motData.firstUsedDate!=='')|| (serviceBookingProcess.motData.manufactureDate && serviceBookingProcess.motData.manufactureDate!=='')){
