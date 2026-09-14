d7d3ca25a6b0d10b9acfc341ca3c5ce06e867324
hynekhruska <43946903+hynekhruska@users.noreply.github.com>
2026-09-14 13:59:46 +0200

    dev

--- serviceBooking/js/work.js
+++ serviceBooking/js/work.js
@@ -1546,7 +1546,7 @@ function showAddingRTSCode(){
 function searchRTSCode(){
     console.log('$("#searchRTSCode").val()',$("#searchRTSCode").val())
     if ($("#searchRTSCode").val()!==''){
-        let r = supportData.rtsCode.filter(el => el.description.includes($("#searchRTSCode").val()!==''));
+        let r = supportData.rtsCode.filter(el => el.description.includes($("#searchRTSCode").val()));
         console.log(r);
         if (r.length>9){
             r = r.slice(0,9);
