d7c161498f3f67eaf9ea06e2001a6ffeaddf856c
hynekhruska <43946903+hynekhruska@users.noreply.github.com>
2026-09-16 18:34:27 +0200

    dev

--- serviceBooking/js/work.js
+++ serviceBooking/js/work.js
@@ -1554,7 +1554,7 @@ async function generateBookingSummary(){
         if (serviceBookingProcess.bookingData.confirmAvailability.selectedMeetAndGreet){
             html += '<br /><b>Create booking</b><br />';
             html += 'Book '+serviceBookingProcess.bookingData.bookingVehicleDescription+' of '+ serviceBookingProcess.customer.FirstName+ ' '+serviceBookingProcess.customer.Surname+' at '+serviceBookingProcess.bookingData.dealerName+' on '+dateToGB(new Date(serviceBookingProcess.bookingData.confirmAvailability.date))+ ' '+serviceBookingProcess.bookingData.confirmAvailability.selectedMeetAndGreet+' for total £' + (serviceBookingProcess.bookingData.discountPercent && serviceBookingProcess.bookingData.discountPercent>0?(total - total*(serviceBookingProcess.bookingData.discountPercent/100)):total).toFixed(2);
-            html += '<a href="#" class="button" onclick="doBookingInAutoline(); return false;">Confirm</a>';
+            html += '<br /><a href="#" class="btn btn-primary" onclick="doBookingInAutoline(); return false;">Confirm</a>';
         }
     }
 
