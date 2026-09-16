b49491d51a980fc183f02275fac88df868363ca7
hynekhruska <43946903+hynekhruska@users.noreply.github.com>
2026-09-16 18:59:39 +0200

    dev

--- serviceBooking/js/work.js
+++ serviceBooking/js/work.js
@@ -1552,7 +1552,7 @@ async function generateBookingSummary(){
                 html += getCourtesyCarsForDate(new Date(serviceBookingProcess.bookingData.confirmAvailability.date)).map(el => el.regNumber+ ' ('+el.vehicleBranch+') - '+el.description).join('<br />') + '<br />'
             }
             if (serviceBookingProcess.bookingData.confirmAvailability.isWaitAvailable){
-                html += '<input type="checkbox" id="bookWaitApointment" onclick="waitAppointmentChange(); return true;"/>Book Wait Appointment';
+                html += '<input type="checkbox" id="bookWaitApointment" onchange="waitAppointmentChange(); return true;"/>Book Wait Appointment';
             }
         }
         if (serviceBookingProcess.bookingData.confirmAvailability.selectedMeetAndGreet){
