2ca01e985c7327de21bad86acd8e41e50173b454
hynekhruska <43946903+hynekhruska@users.noreply.github.com>
2026-09-16 17:04:19 +0200

    dev

--- serviceBooking/js/work.js
+++ serviceBooking/js/work.js
@@ -1590,11 +1590,10 @@ function chooseRTSCode(code){
 
 function addRTSCode(){
     if (!serviceBookingProcess.bookingData.manualPricingLines) serviceBookingProcess.bookingData.manualPricingLines = [];
-    addCodeToBooking('MANUAL#'+$("#addRTSCode").val());
     serviceBookingProcess.bookingData.manualPricingLines.push({code:$("#addRTSCode").val(),name:$("#addDescription").val(),quantity:$("#addQuantity").val(),price:$("#addPrice").val()})
     serviceBookingProcess.bookingData.inAddingRTSCode = false;
     serviceBookingProcess.bookingData.newRTSCode = null;
-    generateBookingSummary();
+    addCodeToBooking('MANUAL#'+$("#addRTSCode").val());
 }
 
 function getCourtesyCarsForDate(date){
