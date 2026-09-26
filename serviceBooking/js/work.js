b400bc8541f816fba1abbf09144fd48f5c309a9e
hynekhruska <43946903+hynekhruska@users.noreply.github.com>
2026-09-26 08:08:40 +0200

    dev

--- serviceBooking/js/work.js
+++ serviceBooking/js/work.js
@@ -1824,6 +1824,8 @@ function doBookingInAutoline(){
     $('#bookServicePricing').hide();
     $('#bookServiceDealership').hide();
     $('#bookingSentToAutoline').show();
+    $('#customerValidation').hide();
+    $('#customerDetails button').hide();
     let html = '<br/><b>Booking is being created in Autoline<b><br/>';
     html += 'Booking details: '+serviceBookingProcess.bookingData.bookingVehicleDescription+' of '+ serviceBookingProcess.customer.FirstName+ ' '+serviceBookingProcess.customer.Surname+' at '+serviceBookingProcess.bookingData.dealerName+' on '+dateToGB(new Date(serviceBookingProcess.bookingData.confirmAvailability.date))+ ' '+(serviceBookingProcess.bookingData.confirmAvailability.selectedMeetAndGreet?serviceBookingProcess.bookingData.confirmAvailability.selectedMeetAndGreet:serviceBookingProcess.bookingData.confirmAvailability.selectedWait)+' for total £' + serviceBookingProcess.bookingData.totalPrice;
     html += (serviceBookingProcess.bookingData.confirmAvailability.selectedWait?'<br />Wait appointment will be booked.':'');
