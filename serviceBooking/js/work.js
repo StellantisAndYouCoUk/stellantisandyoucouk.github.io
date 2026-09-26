6e3b8f34f0158fab49f2de8cf99420b039db14e7
hynekhruska <43946903+hynekhruska@users.noreply.github.com>
2026-09-26 07:40:51 +0200

    dev

--- serviceBooking/js/work.js
+++ serviceBooking/js/work.js
@@ -271,7 +271,7 @@ function chooseDealership(dealershipId){
     }
     $('div[id="bookingProblems"]').hide();
     if (!serviceBookingProcess.bookingData || (serviceBookingProcess.bookingData && serviceBookingProcess.bookingData.knackDealerId !== dealershipId)){
-        serviceBookingProcess.bookingData.pricing = null;
+        if(serviceBookingProcess.bookingData) serviceBookingProcess.bookingData.pricing = null;
         let newDealership = supportData.dealerList.find(el => el.id === dealershipId);
         checkPricingDataForDealership(newDealership);
     }
